e261c87781f68b9d7e21a9d4821257a15a72b5c1
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-06-15 10:12:06 +0200

    Improve code

--- src/ui/setup.py
+++ src/ui/setup.py
@@ -5,6 +5,46 @@ from src.services.service import Service
 from gspread import SpreadsheetNotFound
 
 
+def __display_error(text_body):
+    messagebox.showerror('ERROR', text_body)
+
+
+def __is_empty(text):
+    return len(text.strip()) == 0
+
+
+def __download_file(path, sheet, file_name):
+    path = path.strip()
+    sheet = sheet.strip()
+    file_name = file_name.strip()
+
+    try:
+        if __is_empty(path):
+            __display_error('Output path cannot be empty')
+        elif __is_empty(file_name):
+            __display_error('File name cannot be empty')
+        elif __is_empty(sheet):
+            __display_error('Sheet cannot be empty')
+        else:
+            try:
+                sheet = int(sheet)
+                if sheet < 0:
+                    __display_error('Sheet number cannot be less than 0')
+                else:
+                    try:
+                        service = Service()
+                        service.connect()
+                        service.load_data(sheet, file_name)
+                        service.extract_data_to_file(path)
+                        messagebox.showinfo('COMPLETE', 'Download is complete')
+                    except NotContentReturnedException as e:
+                        messagebox.showwarning('WARNING', e.message)
+            except ValueError:
+                __display_error('The number of the sheet must be a number')
+    except SpreadsheetNotFound:
+        __display_error('The file does not exists')
+
+
 class Setup:
     def __init__(self):
         self.__title = 'Google Sheets Integration'
@@ -26,44 +66,13 @@ class Setup:
         entry_sheet.grid(row=2, column=1, padx=(30, 10), pady=(15, 0), sticky=W)
 
         button = Button(self.__window, text='Download',
-                        command=lambda: self.download_file(entry_path_to_extract.get(),
-                                                           entry_sheet.get(),
-                                                           entry_file_name.get()))
+                        command=lambda: __download_file(entry_path_to_extract.get(),
+                                                        entry_sheet.get(),
+                                                        entry_file_name.get()))
         button.grid(row=3, column=0, columnspan=2, pady=(30, 10))
 
     def display(self):
         self.__window.mainloop()
-
-    def download_file(self, path, sheet, file_name):
-        path = path.strip()
-        sheet = sheet.strip()
-        file_name = file_name.strip()
-
-        try:
-            if self.__is_empty(path):
-                self.__display_error('Output path cannot be empty')
-            elif self.__is_empty(file_name):
-                self.__display_error('File name cannot be empty')
-            elif self.__is_empty(sheet):
-                self.__display_error('Sheet cannot be empty')
-            else:
-                try:
-                    sheet = int(sheet)
-                    if sheet < 0:
-                        self.__display_error('Sheet number cannot be less than 0')
-                    else:
-                        try:
-                            service = Service()
-                            service.connect()
-                            service.load_data(sheet, file_name)
-                            service.extract_data_to_file(path)
-                            messagebox.showinfo('COMPLETE', 'Download is complete')
-                        except NotContentReturnedException as e:
-                            messagebox.showwarning('WARNING', e.message)
-                except ValueError:
-                    self.__display_error('The number of the sheet must be a number')
-        except SpreadsheetNotFound:
-            self.__display_error('The file does not exists')
 
     def __create_label(self, label_text, position, label_row, label_column):
         label = Label(self.__window, text=label_text)
@@ -73,9 +82,3 @@ class Setup:
         entry = Entry(self.__window, width=label_width)
         entry.grid(row=label_row, column=label_column, padx=(30, 10), pady=(15, 0), sticky=label_position)
         return entry
-
-    def __display_error(self, text_body):
-        messagebox.showerror('ERROR', text_body)
-
-    def __is_empty(self, text):
-        return len(text.strip()) == 0
